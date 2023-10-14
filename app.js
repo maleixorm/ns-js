1f6acb59ca8bcbcbcf07b58e8b5e35e521adba1e
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-10-14 12:20:55 -0300

    Criando as condicionais e o loop

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert('Boas vindas ao jogo do numero secreto');
+alert('Seja bem-vindo ao jogo do numero secreto!');
+let numeroSecreto = Math.round(Math.random() * 100);
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um numero entre 1 e 100.');
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o numero secreto: ${numeroSecreto} com ${tentativas} tentativas.`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        tentativas++;
+    }
+}
